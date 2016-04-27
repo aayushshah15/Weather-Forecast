90d07d1287ed1a7e87e4a502e3456de18a6b888d
Aayush Shah <aayush.shah15@gmail.com>
2016-04-27 00:14:10 -0400

    added custom service, configured routes

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 var weather = angular.module('weather', ['ngRoute', 'ngResource']);
+weather.config(function ($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+	.otherwise({
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+});
+weather.service('cityService', function() {
+	this.city = "New York";
+});
+weather.controller('homeController', ['$scope', 'cityService', function ($scope, cityService){
+	$scope.city = cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
+}]);
+weather.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
+}]);
